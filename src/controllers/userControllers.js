d8a90a8d0ecc72a8d823af4c6dc87b7ee028b2e1
Dovile Bartkeviciute <d.bartkeviciute96@gmail.com>
2021-10-25 15:02:22 +0300

    Add bcryptjs and hash new user's password

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -1,12 +1,13 @@
 const { registerUser } = require('../models/userModels');
+const { hashValue } = require('../utils/hashHelper');
 
 const register = async (req, res) => {
-    const dbData = {
+    const newUser = {
         full_name: req.body.full_name,
         email: req.body.email,
-        password: req.body.password
+        password: hashValue(req.body.password)
     }
-    let result = await registerUser(dbData);
+    let result = await registerUser(newUser);
     res.send({msg: 'registration successful', result})
 };
 
